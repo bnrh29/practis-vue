2f4b7937b84fe12cc6162d1235e66637cb4e3f4e
bnrh29 <bnrh29@gmail.com>
2019-01-23 18:11:56 +0900

    vue.js入門の練習です

--- vue.js入門/section5/5-5.js
+++ vue.js入門/section5/5-5.js
@@ -1,78 +1,123 @@
-var MyButton = {
-	props: ['href', 'tag'],
-	render: function (createElement) {
-		var tag = this.tag || (this.href ? 'a' : 'button')
-
-		return createElement(tag, {
-			attrs: {
-				href: this.href || '#'
-			}
-		}, this.$slots.default)
-	},
-/*	template: `
-	<a v-if="(!tag && href) || tag === 'a'" :href="href || '#'">
-		<slot></slot>
-	</a>
-	<span v-else-if="tag === 'span'">
-		<slot></slot>
-	</span>
-	<button v-else>
-		<slot></slot>
-	</button>
-	`
-*/
-}
-
 /*
-new Vue({
-	el: '#app',
-	render: function (createElement) {
-		return createElement('my-button', {
-			attrs: {
-				href: 'https://vuejs.org/'
-			},
-			props: {
-				tag: 'a'
-			}
-		}, 'anchor')
-	},
-	components: {
-		MyButton: MyButton
-	}
-})
-*/
-
-new Vue({
-	el: '#app',
+var IconShareButton = {
+	template: `
+		<button @click="share"><i class="fas fa-share-square"></i></button>
+	`,
 	data: function () {
 		return {
-			counter:0
+			_isProcessing: false
 		}
 	},
-	render: function (createElement) {
-		return createElement(
-			'div',
-			[
-				createElement(
-					'button',
-					{
-						on: {
-							click: () => this.counter += 1
-						}
-					},
-					'クリックでカウントアップ'
-				),
-				createElement(
-					'p',
-					[
-						'クリックされた回数:',
-						createElement(
-							'b',
-							this.counter + ' 回'
-						)
-					]
-				)
-			]
-		)
+	methods: {
+		share: function () {
+			if (this._isProcessing) {
+				return
+			}
+			if (!window.confirm('シェアしますか？')) {
+				return
+			}
+			this._isProcessing = true
+			setTimeout(() => {
+				window.alert('シェアしました')
+				this._isProcessing = false
+			}, 300)
+		}
+	}
+}
+*/
+
+/*
+var TextShareButton = {
+	template: `
+		<button @click="share">{{ buttonLabel }}</button>
+	`,
+	data: function () {
+		return {
+			buttonLabel: 'シェアする',
+			_isProcessing: false
+		}
+	},
+	methods: {
+		share: function () {
+			if (this._isProcessing) {
+				return
+			}
+			if (!window.confirm('シェアしますか？')) {
+				return
+			}
+			this._isProcessing = true
+			setTimeout(() => {
+				window.alert('シェアしました')
+				this._isProcessing = false
+		}, 300)
+		}
+	}
+}
+*/
+
+var Sharable = {
+	data: function () {
+		return {
+			_isProcessing: false
+		}
+	},
+	created: function () {
+		console.log('Sharableミックスインのフックが呼ばれました')
+	},
+	methods: {
+		share: function () {
+			if ( this._isProcessing) {
+				return
+			}
+			if (!window.confirm('シェアしますか？')) {
+				return
+			}
+			this._isProcessing = true
+			setTimeout(() => {
+				window.alert('シェアしました。')
+			this._isProcessing = false
+		}, 300)
+		}
+	}
+}
+
+
+var IconShareButton = {
+	mixins: [Sharable],
+	created: function (){
+		console.log('IconShareButtonのフックが呼ばれました')
+	},
+	template: `
+		<button @click="share"><i class="fas fa-share-square"></i></button>
+	`
+}
+
+
+var TextShareButton = {
+	mixins: [Sharable],
+	template: `
+		<button @click="share">{{ buttonLabel }}</button>
+	`,
+	created: function () {
+		console.log('TextShareButtonのフックが呼ばれました')
+	},
+	methods: {
+		share () {
+			window.alert('コンポーネントからシェアしました。')
+		}
+	},
+	data: function () {
+		return {
+			buttonLabel: 'シェアする'
+		}
+	}
+}
+
+
+new Vue({
+	el: '#app',
+	components: {
+		IconShareButton,
+		TextShareButton
 	}
 })
